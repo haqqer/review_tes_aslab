24e6645d962b93f7d61d3c3fa66bfdcd802a5f7d
haqqer <ilhamserbaguna@gmail.com>
2018-09-05 19:49:53 +0700

    diganti folder 2

--- README.md
+++
@@ -1,3 +0,0 @@
-﻿# Hasil Review Tes Aslab 
-## Folder Soal 2
-Soal nomor 2 membuat web untuk menginput dan menghitung tagihan kuliah dengan ketentuan2 tertentu 
--- tagihan/proses_isi.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once('../database.php');
-$nim = $_POST['nim'];
-$query = mysqli_query($conn, "SELECT * FROM tb_mhs WHERE nim='$nim'");
-$data = mysqli_fetch_array($query);
- 
-echo json_encode($data);
